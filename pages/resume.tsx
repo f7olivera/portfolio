1e8c3335ad22b2c532989c162d7acc1892fbf3dc
f7olivera <f7olivera@gmail.com>
2022-11-16 00:05:05 -0300

    Update resume.tsx

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 const Resume: NextPage = () => {
   return (
     <object data='/Facundo Olivera CV.pdf'
+        style={{ height: '100vh'}}
         type='application/pdf'
-        width='100%'
-        height='700px' />
+        width='100%'/>
   )
 }
 
